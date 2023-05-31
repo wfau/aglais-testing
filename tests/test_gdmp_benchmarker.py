7dfc297b0f208f537c1f1b49a54ea086326355a1
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2023-05-31 22:32:06 +0300

    Modified docs on test class

--- tests/test_gdmp_benchmarker.py
+++ tests/test_gdmp_benchmarker.py
@@ -4,29 +4,30 @@ import unittest.mock as mock
 
 """
 Main functionalities:
-The GDMPBenchmarker class is responsible for running Zeppelin notebooks and benchmarking their performance. It can run notebooks in parallel for multiple users, compare their output to expected output, and delete the notebooks after they have been run. It also generates user configuration files for Zeppelin authentication.
+The GDMPBenchmarker class is responsible for benchmarking Zeppelin notebooks. It allows users to run notebooks and compare their output against expected output. The class can run notebooks in parallel, and it can delete the notebooks after they have been run. The class also generates user configurations for Zeppelin, and it can validate the configuration passed in by the user.
 
 Methods:
-- get_note: retrieves a Zeppelin notebook from a URL and returns it as a dictionary
-- generate_zdairi_user_configs: generates user configuration files for Zeppelin authentication based on a user configuration file
-- _delete_notebook: deletes a Zeppelin notebook using the zdairi command-line tool
-- _create_notebook: creates a Zeppelin notebook using the zdairi command-line tool and returns its ID
-- _run_notebook: runs a Zeppelin notebook using the zdairi command-line tool and returns its output, status, and messages
-- _get_user_config: returns the path to a user configuration file based on whether the method is being run concurrently or not
-- run_notebook: runs a Zeppelin notebook and returns its results, including output, status, and timing
-- run: runs Zeppelin notebooks for multiple users, either in parallel or sequentially, and returns their results
+- __init__: Initializes the GDMPBenchmarker object with user configuration, Zeppelin URL, verbosity, notebook handler, and a list of notebooks.
+- get_note: Sends a GET request to a Zeppelin notebook URL and returns the response as a dictionary.
+- generate_zdairi_user_configs: Generates user configurations for Zeppelin based on a user configuration file.
+- _get_user_config: Returns the path to the user configuration file for a given process.
+- _write_data_to_file: Writes data to a file with a given filepath.
+- run_notebook: Runs a Zeppelin notebook and returns the results.
+- run: Runs a list of notebooks and returns the results.
+- _run_parallel: Runs a list of notebooks in parallel and returns the results.
+- _validate_output: Compares actual output to expected output and returns a tuple indicating whether the output is valid, the result status message, and the output message.
+- _run_single: Runs a list of notebooks sequentially and returns the results.
 
 Fields:
-- DEFAULT_DIR: the default directory for user configuration files
-- DEFAULT_USER_CONFIG: the default user configuration file
-- verbose: whether to print verbose output
-- zeppelin_url: the URL of the Zeppelin instance
-- userconfig: the path to the user configuration file
-- notebooks: a list of Notebook objects
-- default_userconfig: the default user configuration file
-- expected_output: the expected output of the notebook
-- expectedtime: the expected time for the notebook to run
-- total_users: The total users 
+- DEFAULT_DIR: The default directory for user configuration files.
+- DEFAULT_USER_CONFIG: The default user configuration file.
+- verbose: Whether to print verbose output.
+- zeppelin_url: The URL for the Zeppelin instance.
+- userconfig: The path to the user configuration file.
+- notebooks: A list of notebooks to run.
+- default_userconfig: The default user configuration file.
+- total_users: The total number of users generated from the user configuration file.
+- notebook_handler: The notebook handler to use for running notebooks.
 """
 
 class TestGDMPBenchmarker(unittest.TestCase):
